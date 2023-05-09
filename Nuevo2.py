68446051415b3c1cc8b3fd6eef4ce300a7ad47fe
Markpo2 <127012533+Markpo2@users.noreply.github.com>
2023-05-08 23:27:12 -0500

    Update Nuevo2.py

--- Nuevo2.py
+++ Nuevo2.py
@@ -1,5 +1,6 @@
 import os
 import telegram
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
 from urllib.request import urlretrieve
 
 # Configura el token de autenticación del bot
@@ -9,11 +10,11 @@ TOKEN = '6043283784:AAHLV11M9g3gaDj5dE-Sr5fKhSCd8CT-lOc'
 bot = telegram.Bot(token=TOKEN)
 
 # Define una función para manejar los mensajes enviados al bot
-def handle_message(update, context):
+def handle_message(update: Update, context):
     message = update.message
     text = message.text
     
-    # Si el mensaje es un enlace de descarga que contiene "mkv" o "mp4"
+    # Si el mensaje es un enlace de descarga que contiene "zip" o "rar" o "mkv" o "mp4"
     if text.startswith('http') and ('.zip' in text or '.rar' in text or '.mkv' in text or '.mp4' in text):
         # Descarga el archivo desde el enlace
         file_name, headers = urlretrieve(text)
@@ -30,10 +31,16 @@ def handle_error(update, context):
     error = context.error
     print(f'Error: {error}')
 
-# Crea un objeto de despachador para registrar las funciones de manejo de mensajes y errores
-dispatcher = telegram.ext.Dispatcher(bot, None)
-dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
-dispatcher.add_error_handler(handle_error)
+# Crea un objeto de actualización para recibir las actualizaciones del bot
+updater = telegram.Updater(TOKEN, use_context=True)
+
+# Registra la función de manejo de mensajes y errores con el objeto de actualización
+updater.dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
+updater.dispatcher.add_error_handler(handle_error)
 
 # Inicia el bot y espera a que lleguen nuevos mensajes
-bot.polling()
+updater.start_polling()
+
+# Agrega un bucle while infinito para mantener el bot en ejecución
+while True:
+    pass
